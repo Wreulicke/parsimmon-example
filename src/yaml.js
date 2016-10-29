c040a56e0e1c426500cf24963e5424923478f35a
Wreulicke <masaya2dies@gmail.com>
2016-10-29 18:07:15 +0900

    initial commit

---
+++ src/yaml.js
@@ -0,0 +1,37 @@
+'use strict'
+const P = require('parsimmon')
+const alt = P.alt
+const string = P.string
+const space = string(' ').atLeast(0).map((r) => r.join(''))
+const indent = (n, parser) => (n === 0) ? parser : string('  '.repeat(n)).then(parser)
+const identifier = (P.regex(/[a-z]+/i))
+const linebreak = alt(string("\r\n"), string("\n"), string('\r'))
+
+const nest = (n) => linebreak.chain((r) => yaml(n + 1))
+const node = (n) => P.seq(
+  identifier.skip(P.optWhitespace),
+  string(':').then(space).then(alt(nest(n), identifier)),
+  linebreak.atLeast(0)
+)
+const yaml = (n) => 
+  indent(n, node(n)).map((v)=>{return {key:v[0], value:v[1]}})
+  .many().map((v) => v.reduce((res, arr) => {
+    return res[arr.key] = arr.value, res
+  }, {}))
+
+const parser = yaml(0)
+
+module.exports = parser
+
+const str=[
+"aaa : bbb",
+"aaab : ccc",
+"bbb: sss",
+"ccc:",
+"  xxx:bbb",
+"  xxc:bbb",
+"  xxz:",
+"    xxx:bbb",
+"    xxc:bbb"
+].join("\r\n")
+console.log(JSON.stringify(parser.parse(str).value, null, 2))
